0192e410fcfef687047110739d4f7665d3e5aa29
vijayta <vijayta.panchal@gmail.com>
2014-08-19 14:35:33 +0530

    design done

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,25 +13,25 @@ class User < ActiveRecord::Base
     REFERRAL_STEPS = [
         {
             'count' => 5,
-            "html" => "Shave<br>Cream",
+            "html" => "Early access<br />to pre-sale",
             "class" => "two",
             "image" =>  ActionController::Base.helpers.asset_path("refer/cream-tooltip@2x.png")
         },
         {
             'count' => 10,
-            "html" => "Truman Handle<br>w/ Blade",
+            "html" => "Free shipping<br />on your first order",
             "class" => "three",
             "image" => ActionController::Base.helpers.asset_path("refer/truman@2x.png")
         },
         {
             'count' => 25,
-            "html" => "Winston<br>Shave Set",
+            "html" => "$10 off your<br />first order",
             "class" => "four",
             "image" => ActionController::Base.helpers.asset_path("refer/winston@2x.png")
         },
         {
             'count' => 50,
-            "html" => "One Year<br>Free Blades",
+            "html" => "<strong>FREE DRESS!</strong><br>The first 10 insiders to<br />invite 50 friends will<br />receive a free Brass<br />dress. Cuz we <span class='heart'>&#9829;</span> you.",
             "class" => "five",
             "image" => ActionController::Base.helpers.asset_path("refer/blade-explain@2x.png")
         }
